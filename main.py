97a9d9e4e7a3245f48d1bec7148acb0445c8a3e9
John Chidgey <john.chidgey@me.com>
2021-08-15 19:34:25 +1000

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,29 @@ import base64
 import pycurl
 
 # Definitions
-CACHELIMIT = 10000
 TLVS = ["7629171", "7629169", "133773310"]
 # TLVS_TO_EXTRACT = [["action", "boost"], ["podcast", "Analytical"]]
 # TLVS_TO_EXTRACT = [["action", "boost"]]
 TLVS_TO_EXTRACT = [["message", ""]]
+DEFAULT_NUM_MAX_INVOICES = 10000
+DEFAULT_INDEX_OFFSET = 0
+REMEMBER_LAST_INDEX = True
+BOOSTAGRAM_FIELDS_TO_PUSH = ["app_name", "podcast", "episode", "message", "sender_name"]
 
 # Pushover Notifications
-PUSHOVER_ENABLE = 0
-#PUSHOVER_USER_TOKEN = "<TOKEN HERE>"
-#PUSHOVER_API_TOKEN = "<TOKEN HERE>"
+PUSHOVER_ENABLE = True
 PUSHOVER_USER_TOKEN = "<TOKEN HERE>"
 PUSHOVER_API_TOKEN = "<TOKEN HERE>"
 
+def JSONtoString(JSONObject):
+    message_string = 'Boost-A-Gram recieved, '
+    for key, value in JSONObject.items():
+        if key in BOOSTAGRAM_FIELDS_TO_PUSH:
+            message_string += (key + ' = ' + str(value) + ', ')
+    message_string = message_string[:-2]
+    return message_string
+
+# End JSONtoString
 
 def main():
     os.environ["GRPC_SSL_CIPHER_SUITES"] = 'HIGH+ECDSA'
@@ -43,73 +53,77 @@ def main():
     creds = grpc.ssl_channel_credentials(cert)
     channel = grpc.secure_channel('localhost:10009', creds)
     stub = lnrpc.LightningStub(channel)
-    request = ln.ListInvoiceRequest(
-        pending_only=False,
-        index_offset=0,
-        num_max_invoices=10000,
-        reversed=False,
-    )
+
+    if REMEMBER_LAST_INDEX:
+        # Open and read in previous index position
+        invoicefile = os.path.expanduser('lastinvoice.db')
+        if os.path.exists(invoicefile):
+            with open(invoicefile) as invoicefiledata:
+                invoicedataraw = invoicefiledata.readlines()
+                indexarray = [i.strip() for i in invoicedataraw]
+                indexoffset = int(indexarray[0])
+
+        request = ln.ListInvoiceRequest(
+            pending_only=False,
+            index_offset=indexoffset,
+            num_max_invoices=DEFAULT_NUM_MAX_INVOICES,
+            reversed=False,
+        )
+        invoice_database_index = indexoffset
+    else:
+        request = ln.ListInvoiceRequest(
+            pending_only=False,
+            index_offset=DEFAULT_INDEX_OFFSET,
+            num_max_invoices=DEFAULT_NUM_MAX_INVOICES,
+            reversed=False,
+        )
+        invoice_database_index = DEFAULT_INDEX_OFFSET
 
     # Retrieve and display the wallet balance
     invoice_list = stub.ListInvoices(request, metadata=[('macaroon', macaroon)])
     serialized_invoice_list = MessageToJson(invoice_list)
     dictionary_invoice_list = json.loads(serialized_invoice_list)
-    invoice_database_index = 0
+    if len(dictionary_invoice_list):
+        for invoice in dictionary_invoice_list['invoices']:
+            if "htlcs" in invoice and "isKeysend" in invoice:
+                for htlc in invoice['htlcs']:
+                    if "customRecords" in htlc:
+                        for tlv_index, tlv_payload in htlc['customRecords'].items():
+                            if tlv_index in TLVS:
+                                tlv_payload_UTF8 = base64.b64decode(tlv_payload).decode('utf8')
+                                pushover_message = ''
+                                try:
+                                    tlv_payload_decoded = json.loads(tlv_payload_UTF8)
+                                    for key, value in TLVS_TO_EXTRACT:
+                                        if tlv_payload_decoded[key] in value:
+                                            pushover_message = JSONtoString(tlv_payload_decoded)
+                                        elif tlv_payload_decoded[key] and not value:
+                                            pushover_message = JSONtoString(tlv_payload_decoded)
+                                except:
+                                    pass
 
-    for invoice in dictionary_invoice_list['invoices']:
-        value_for_value = False
-        creationDate = ""
-#        if "creationDate" in invoice:
-#            creationDate = invoice['creationDate']
-        keysend = False
-        if "isKeysend" in invoice:
-            keysend = True
-        if "htlcs" in invoice and keysend:
-            for htlc in invoice['htlcs']:
-                if "customRecords" in htlc:
-                    for tlv_index, tlv_payload in htlc['customRecords'].items():
-                        if tlv_index in TLVS:
-                            tlv_payload_decoded = ''
-                            tlv_payload_UTF8 = base64.b64decode(tlv_payload).decode('utf8')
-                            try:
-                                tlv_payload_decoded = json.loads(tlv_payload_UTF8)
-                                for key, value in TLVS_TO_EXTRACT:
-                                    if tlv_payload_decoded[key] in value:
-                                        print(tlv_payload_decoded)
-                                        if PUSHOVER_ENABLE:
-                                            crl = pycurl.Curl()
-                                            crl.setopt(crl.URL, 'https://api.pushover.net/1/messages.json')
-                                            crl.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json' , 'Accept: application/json'])
-                                            data = json.dumps({"token": PUSHOVER_API_TOKEN, "user": PUSHOVER_USER_TOKEN, "title": "RSS Notifier", "message": thefeedentry.get("title", "") + " Now Live"})
-                                            crl.setopt(pycurl.POST, 1)
-                                            crl.setopt(pycurl.POSTFIELDS, data)
-                                            crl.perform()
-                                            crl.close()
- 
-                                   elif tlv_payload_decoded[key] and not value:
-                                        print(tlv_payload_decoded)
-                            except:
-                                pass
+                                if PUSHOVER_ENABLE and pushover_message:
+                                    crl = pycurl.Curl()
+                                    crl.setopt(crl.URL, 'https://api.pushover.net/1/messages.json')
+                                    crl.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json' , 'Accept: application/json'])
+                                    data = json.dumps({"token": PUSHOVER_API_TOKEN, "user": PUSHOVER_USER_TOKEN, "title": "RSS Notifier", "message": pushover_message })
+                                    crl.setopt(pycurl.POST, 1)
+                                    crl.setopt(pycurl.POSTFIELDS, data)
+                                    crl.perform()
+                                    crl.close()
 
-        invoice_database_index += 1
+            invoice_database_index += 1
 # End For Loop
 
 # Boilerplate for flagging current database index for reduce seek times in future TBD
-    # Open and read in all previously found Invoices
-    cachefile = os.path.expanduser('~/invoicelist.db')
-    if os.path.exists(cachefile):
-        with open(cachefile) as dbdsc:
-            dbfromfile = dbdsc.readlines()
-        dblist = [i.strip() for i in dbfromfile]
-        dbfeed = deque(dblist, CACHELIMIT)
-    else:
-        dbfeed = deque([], CACHELIMIT)
-
     # Check for new Invoices
 
-    # Write all previous and current Invoices
-    with open(cachefile, 'w') as dbdsc:
-        dbdsc.writelines((''.join([i, os.linesep]) for i in dbfeed))
+        # Write all previous and current Invoices
+        # Open and read in previous index position
+        invoicefile = os.path.expanduser('lastinvoice.db')
+        if os.path.exists(invoicefile):
+            with open(invoicefile, 'w') as invoicefiledata:
+                invoicefiledata.write(str(invoice_database_index))
 
 # End
 
